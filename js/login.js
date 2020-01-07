79d3aab22938b102e2b70b418c8b7aa5796bd1f0
raphaelabl <55430786+raphaelabl@users.noreply.github.com>
2020-01-07 15:50:15 +0100

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function getInputValue(){
+
+  var emailInput = document.getElementById("email_input_text").value;
+  var passwordInput = document.getElementById("password_input_text").value
+  var inhalt = "";
+
+  if(emailInput == "" || passwordInput == ""){
+    alert("Abc");
+  }
+  else{
+
+  }
+}
