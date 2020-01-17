72a2fcb84e0011691fdaebec6f6c7193fb4c3727
raphaelabl <raphael.ablinger@gmx.at>
2020-01-17 08:32:35 +0100

    Sql-Database with Nodejs server

--- js/login.js
+++ js/login.js
@@ -1,13 +1,38 @@
-function getInputValue(){
+const express  = require('express');
+const app = express();
 
-  var emailInput = document.getElementById("email_input_text").value;
-  var passwordInput = document.getElementById("password_input_text").value
-  var inhalt = "";
+var mysql = require('mysql');
+var path = require('path');
 
-  if(emailInput == "" || passwordInput == ""){
-    alert("Daten wurden abgesendet");
-  }
-  else{
+app.use(express.urlencoded({extended: true}));
 
-  }
+app.get('/', function(req, res){
+  res.sendFile(path.join("C:/Users/ablin/OneDrive/Dokumente/GitHub/Website/login.html"));
+});
+
+function insert(username, password){
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Apschi2001",
+    database: "mydb"
+  });
+
+  con.connect(function(err){
+    if(err) throw err;
+  });
+
+  con.query("INSERT INTO customers (name, password) VALUES (?, ?)", [username, password], function(err, result){
+    if(err) throw err;
+  });
+  con.end();
 }
+
+app.post('/login-backend', function(req, res){
+  res.send(req.body.username_input + " " + req.body.password_input)
+  insert(req.body.username_input ,req.body.password_input);
+});
+
+const server = app.listen(80, function(){
+  console.log("Server started");
+});
