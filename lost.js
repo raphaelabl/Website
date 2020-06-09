1b5112aa5764b94ee1a4ffa61787358332243f41
raphaelabl <raphael.ablinger@gmx.at>
2020-06-09 08:06:57 +0200

    LOST

--- lost.js
+++ lost.js
@@ -1,9 +1,12 @@
+var path = require('path');
+
 const express  = require('express');
 const app = express();
 
-console.log(__dirname);
 app.use(express.static(__dirname + '/'));
 
-app.listen(1337, function(){
-    console.log('Listening at Port 1337');
+app.listen(1111, function(){
+    console.log('Listening at Port 1111');
 });
+
+//app.use(express.urlencoded({extended: true}));
