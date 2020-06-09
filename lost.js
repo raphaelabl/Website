878cd44ca6299e54ef84c789e284ec3fea34cab2
christophhandel <55431396+christophhandel@users.noreply.github.com>
2020-06-09 08:30:46 +0200

    new

--- lost.js
+++ lost.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express  = require('express');
-const app = express();
-
-console.log(__dirname);
-app.use(express.static(__dirname + '/'));
-
-app.listen(1337, function(){
-    console.log('Listening at Port 1337');
-});
-=======
 var path = require('path');
 
 const express  = require('express');
@@ -21,4 +10,3 @@ app.listen(1111, function(){
 });
 
 //app.use(express.urlencoded({extended: true}));
->>>>>>> 1b5112aa5764b94ee1a4ffa61787358332243f41
