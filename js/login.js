598f6338f8596259bcbe17e4854e98dfd3bb854a
raphaelabl <raphael.ablinger@gmx.at>
2020-06-09 08:24:43 +0200

    lasdfn

---
+++ js/login.js
@@ -0,0 +1,5 @@
+var niklas = document.getElementById("test");
+
+function InputName(){
+  niklas.innerHTML += "<br>Apschi ist (sch)cool";
+}
--- lost.js
+++ lost.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const express  = require('express');
 const app = express();
 
@@ -11,14 +12,23 @@ app.listen(1337, function(){
 =======
 var path = require('path');
 
+=======
+>>>>>>> parent of 1b5112a... LOST
 const express  = require('express');
 const app = express();
 
+console.log(__dirname);
 app.use(express.static(__dirname + '/'));
 
+<<<<<<< HEAD
 app.listen(1111, function(){
     console.log('Listening at Port 1111');
 });
 
 //app.use(express.urlencoded({extended: true}));
 >>>>>>> 1b5112aa5764b94ee1a4ffa61787358332243f41
+=======
+app.listen(1337, function(){
+    console.log('Listening at Port 1337');
+});
+>>>>>>> parent of 1b5112a... LOST
