9f29ef7236b76800c7fcb522102521d1814a17d8
raphaelabl <raphael.ablinger@gmx.at>
2020-06-09 08:23:36 +0200

    oh

--- lost.js
+++ lost.js
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+const express  = require('express');
+const app = express();
+
+console.log(__dirname);
+app.use(express.static(__dirname + '/'));
+
+app.listen(1337, function(){
+    console.log('Listening at Port 1337');
+});
+=======
 var path = require('path');
 
 const express  = require('express');
@@ -10,3 +21,4 @@ app.listen(1111, function(){
 });
 
 //app.use(express.urlencoded({extended: true}));
+>>>>>>> 1b5112aa5764b94ee1a4ffa61787358332243f41
