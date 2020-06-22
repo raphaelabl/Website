bb2b3013dbb0521b00e77717ebbd55be6b494d30
raphaelabl <raphael.ablinger@gmx.at>
2020-06-22 19:12:00 +0200

    ka

--- lost.js
+++ lost.js
@@ -1,12 +1,68 @@
-var path = require('path');
+var app = angular
+  .module("myApp", [])
 
-const express  = require('express');
-const app = express();
+  .controller("mainController", function (
+    $scope,
+    $rootScope,
+    $timeout,
+    $interval
+  ) {
+    $scope.poll_title = "What is your favorite animal?";
+    $scope.isButton = true;
+    $scope.options = [
+      { name: "Panda", votes: 0, pct: 10 },
+      { name: "Iguana", votes: 0, pct: 90 },
+      { name: "Grizzly Bear", votes: 0, pct: 0 },
+      { name: "Ostrich", votes: 0, pct: 0 },
+      { name: "Leopard", votes: 0, pct: 0 },
+      { name: "Fox", votes: 0, pct: 0 }
+    ];
+    $scope.totalVotes = sumVotes();
 
-app.use(express.static(__dirname + '/'));
+    $scope.addVote = function (opt, index) {
+      if ($scope.isButton) {
+        $scope.isButton = false;
+        opt.votes += 1;
+        $scope.totalVotes = sumVotes();
+        updateRowVotes();
+        closeBooth(index);
+      }
+    };
 
-app.listen(1111, function(){
-    console.log('Listening at Port 1111');
-});
+    function sumVotes() {
+      var votes = 0;
+      for (var key in $scope.options) {
+        if ($scope.options.hasOwnProperty(key)) {
+          votes += $scope.options[key].votes;
+        }
+      }
+      return votes;
+    }
 
-//app.use(express.urlencoded({extended: true}));
+    function updateRowVotes() {
+      for (var key in $scope.options) {
+        if ($scope.options.hasOwnProperty(key)) {
+          var obj = $scope.options[key];
+          console.log($scope.totalVotes);
+          obj.pct = (obj.votes / $scope.totalVotes) * 100;
+        }
+      }
+    }
+
+    function closeBooth(clicked) {
+      $(".vote-row").each(function (index) {
+        if (clicked !== index) {
+          $(this).find("button.vote-btn").css("transform", "scale(0)");
+          $(this).find("button.vote-btn").text("");
+        } else {
+          $(this).find("button.vote-btn").text("");
+          $(this)
+            .find("button.vote-btn")
+            .css({ "border-radius": "100%", padding: "7px 10px" });
+          $(this)
+            .find("button.vote-btn")
+            .append("<i class='checkup fa fa-check' aria-hidden='true'></i>");
+        }
+      });
+    }
+  });
